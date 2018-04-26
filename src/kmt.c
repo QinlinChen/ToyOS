186c123ac1f997e33fdcd890b36e5c07c11dd354
tracer <tracer@tracer.org>
2018-04-26 11:23:42 +0800

    > build ray Linux ubuntu 4.10.0-28-generic #32~16.04.2-Ubuntu SMP Thu Jul 20 10:19:48 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/kmt.c
+++ src/kmt.c
@@ -1,4 +1,77 @@
 #include <os.h>
 
+static void kmt_init();
+static int kmt_create(thread_t *thread, void (*entry)(void *arg), void *arg);
+static void kmt_teardown(thread_t *thread);
+static void thread_t *kmt_schedule();
+static void kmt_spin_init(spinlock_t *lk, const char *name);
+static void kmt_spin_lock(spinlock_t *lk);
+static void kmt_spin_unlock(spinlock_t *lk);
+static void kmt_sem_init(sem_t *sem, const char *name, int value);
+static void kmt_sem_wait(sem_t *sem);
+static void kmt_sem_signal(sem_t *sem);
+
 MOD_DEF(kmt) {
+    .init = kmt_init,
+    .create = kmt_create,
+    .teardown = kmt_teardown,
+    .schedule = kmt_schedule,
+    .spin_init = kmt_sem_init,
+    .spin_lock = kmt_spin_lock,
+    .spin_unlock = kmt_spin_unlock,
+    .sem_init = kmt_sem_init,
+    .sem_wait = kmt_sem_wait,
+    .sem_signal = kmt_sem_signal,
 };
+
+struct thread {
+    int i;
+};
+
+// struct spinlock {
+    
+// };
+
+// struct semaphore{
+    
+// };
+
+static void kmt_init() {
+
+}
+
+static int kmt_create(thread_t *thread, void (*entry)(void *arg), void *arg) {
+    return 0;
+}
+
+static void kmt_teardown(thread_t *thread) {
+
+}
+
+static void thread_t *kmt_schedule() {
+
+}
+
+static void kmt_spin_init(spinlock_t *lk, const char *name) {
+
+}
+
+static void kmt_spin_lock(spinlock_t *lk) {
+
+}
+
+static void kmt_spin_unlock(spinlock_t *lk) {
+
+}
+
+static void kmt_sem_init(sem_t *sem, const char *name, int value) {
+
+}
+
+static void kmt_sem_wait(sem_t *sem) {
+
+}
+
+static void kmt_sem_signal(sem_t *sem) {
+
+}
