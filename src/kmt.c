2c7a14165af90bcae14cc59dc564eda750a02ebe
tracer <tracer@tracer.org>
2018-04-26 11:36:52 +0800

    > build ray Linux ubuntu 4.10.0-28-generic #32~16.04.2-Ubuntu SMP Thu Jul 20 10:19:48 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux

--- src/kmt.c
+++ src/kmt.c
@@ -1,9 +1,10 @@
 #include <os.h>
+#include <common.h>
 
 static void kmt_init();
 static int kmt_create(thread_t *thread, void (*entry)(void *arg), void *arg);
 static void kmt_teardown(thread_t *thread);
-static void thread_t *kmt_schedule();
+static thread_t *kmt_schedule();
 static void kmt_spin_init(spinlock_t *lk, const char *name);
 static void kmt_spin_lock(spinlock_t *lk);
 static void kmt_spin_unlock(spinlock_t *lk);
@@ -12,66 +13,68 @@ static void kmt_sem_wait(sem_t *sem);
 static void kmt_sem_signal(sem_t *sem);
 
 MOD_DEF(kmt) {
-    .init = kmt_init,
-    .create = kmt_create,
-    .teardown = kmt_teardown,
-    .schedule = kmt_schedule,
-    .spin_init = kmt_spin_init,
-    .spin_lock = kmt_spin_lock,
-    .spin_unlock = kmt_spin_unlock,
-    .sem_init = kmt_sem_init,
-    .sem_wait = kmt_sem_wait,
-    .sem_signal = kmt_sem_signal,
+  .init = kmt_init,
+  .create = kmt_create,
+  .teardown = kmt_teardown,
+  .schedule = kmt_schedule,
+  .spin_init = kmt_spin_init,
+  .spin_lock = kmt_spin_lock,
+  .spin_unlock = kmt_spin_unlock,
+  .sem_init = kmt_sem_init,
+  .sem_wait = kmt_sem_wait,
+  .sem_signal = kmt_sem_signal,
 };
 
 struct thread {
-    int i;
+  int TODO;
 };
 
-// struct spinlock {
-    
-// };
+struct spinlock {
+  int TODO;
+};
 
-// struct semaphore{
-    
-// };
+struct semaphore {
+  int TODO;
+};
 
 static void kmt_init() {
-
+  Panic("TODO");
 }
 
 static int kmt_create(thread_t *thread, void (*entry)(void *arg), void *arg) {
-    return 0;
+  Panic("TODO");
+  return 0;
 }
 
 static void kmt_teardown(thread_t *thread) {
-
+  Panic("TODO");
 }
 
-static void thread_t *kmt_schedule() {
-    return NULL;
+static thread_t *kmt_schedule() {
+  Panic("TODO");
+  return NULL;
 }
 
 static void kmt_spin_init(spinlock_t *lk, const char *name) {
-
+  Panic("TODO");
 }
 
 static void kmt_spin_lock(spinlock_t *lk) {
-
+  Panic("TODO");
 }
 
 static void kmt_spin_unlock(spinlock_t *lk) {
-
+  Panic("TODO");
 }
 
 static void kmt_sem_init(sem_t *sem, const char *name, int value) {
-
+  Panic("TODO");
 }
 
 static void kmt_sem_wait(sem_t *sem) {
-
+  Panic("TODO");
 }
 
 static void kmt_sem_signal(sem_t *sem) {
-
+  Panic("TODO");
 }
