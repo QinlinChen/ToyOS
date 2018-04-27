4cc6987b4a091f352fad64a1285d85a74a858aaf
tracer <tracer@tracer.org>
2018-04-27 20:39:27 +0800

    > run ray Linux ubuntu 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:10 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/test.c
+++ src/test.c
@@ -154,8 +154,8 @@ void lock_test() {
   kmt->create(&c, addsum, (void *)N);
 }
 
-sem_t empty = SEM_INIT("sem_empty", 1);
-sem_t fill = SEM_INIT("sem_fill", 0);
+sem_t empty;
+sem_t fill;
 
 static void producer(void *arg) {
   while (1) {
@@ -173,14 +173,14 @@ static void consumer(void *arg) {
   }
 }
 
-void sem_test() {
+void sem_test(int N) {
   thread_t a, b;
-  // kmt->sem_init(&empty, "sem_empty", N);
-  // kmt->sem_init(&fill, "sem_fill", 0);
+  kmt->sem_init(&empty, "sem_empty", N);
+  kmt->sem_init(&fill, "sem_fill", 0);
   kmt->create(&a, producer, NULL);
   kmt->create(&b, consumer, NULL);
 }
 
 void test() {
-  sem_test();
+  sem_test(3);
 }
