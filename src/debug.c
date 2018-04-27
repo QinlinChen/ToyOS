2c047c690b8da9e496c63d129a2a52e9edc771fb
tracer <tracer@tracer.org>
2018-04-27 23:03:32 +0800

    > run ray Linux ubuntu 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:10 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/debug.c
+++ src/debug.c
@@ -2,8 +2,8 @@
 
 void fence_check(uint8_t *fence) {
   for (int i = 0; i < FENCESIZE; ++i)
-    if (fence[i] == FENCEBYTE) 
-      Panic("[ERROR] Stack overflow at %p", fence);
+    if (fence[i] != FENCEBYTE) 
+      Panic("[ERROR] Stack overflow at %p", &fence[i]);
 }
 
 void fence_set(uint8_t *target) {
