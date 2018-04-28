daf916d1f9ce2b13a04fdf527b29a19233a1f919
tracer <tracer@tracer.org>
2018-04-28 08:18:57 +0800

    > run ray Linux ubuntu 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:10 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/klib.c
+++ src/klib.c
@@ -175,13 +175,10 @@ static int print_char(char ch,
     return 0;
 }
 
-spinlock_t lock = SPINLOCK_INIT("print_lock");
-
 int printf(const char *fmt, ...) {
   char flag, length;
   int width, prec, error;
   const char *mark;
-  kmt->spin_lock(&lock);
   va_list ap;
   va_start(ap, fmt);
 
@@ -244,7 +241,6 @@ int printf(const char *fmt, ...) {
   }
 
   va_end(ap);
-  kmt->spin_lock(&lock);
   return 0;
 }
 
