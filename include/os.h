ad4ac7d6a1ec9c5ee946bb1deffd9ae526676ae9
tracer <tracer@tracer.org>
2018-05-03 14:56:38 +0800

    > build ray Linux ubuntu 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:10 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- include/os.h
+++ include/os.h
@@ -3,6 +3,57 @@
 
 #include <kernel.h>
 
+// Thread 
+#define PGSIZE            4096
+#define MAX_KSTACK_SIZE   4 * PGSIZE 
+#define MAX_TIMESLICE     2
+
+enum { RUNNABLE, RUNNING, BLOCKED, DEAD };
+
+struct thread {
+  int tid;
+  int stat;
+  int timeslice;
+  uint8_t *kstack;
+  _RegSet *regs;
+  struct thread *next;
+};
+
+struct spinlock {
+  int locked;
+  const char *name;
+};
+
+#define SPINLOCK_INIT(NAME) \
+  (struct spinlock) { \
+    .locked = 0, \
+    .name = (NAME), \
+  }
+
+typedef struct _threadqueue_node {
+  struct thread *thread;
+  struct _threadqueue_node *next;
+} threadqueue_node;
+
+typedef struct _threadqueue {
+  threadqueue_node *head;
+  threadqueue_node *tail;
+  int size;
+} threadqueue;
+
+struct semaphore {
+  int count;
+  threadqueue queue;
+  struct spinlock lock;
+};
+
+#define SEM_INIT(NAME, VALUE) \
+  (struct semaphore) { \
+    .count = (VALUE), \
+    .queue = { NULL, NULL, 0 }, \
+    .lock = { 0, (NAME) }, \
+  }
+  
 static inline void puts(const char *p) {
   for (; *p; p++) {
     _putc(*p);
