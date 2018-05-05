e3d52bc45f80b43df341015807c2274a143287d1
tracer <tracer@tracer.org>
2018-05-05 15:58:25 +0800

    > run ray Linux ubuntu 4.13.0-39-generic #44~16.04.1-Ubuntu SMP Thu Apr 5 16:43:10 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/kmt.c
+++ src/kmt.c
@@ -182,7 +182,7 @@ static void kmt_teardown(thread_t *thread) {
 }
 
 static thread_t *kmt_schedule() {
-  threadlist_print();
+  // threadlist_print();
   Assert(current != NULL);
   thread_t *scan;
 
