3fd488a7dcb11ca3a6424fac160a9a0823ae872a
tracer <tracer@tracer.org>
2018-05-30 23:01:48 +0800

    > clean ray Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- include/debug.h
+++ include/debug.h
@@ -6,7 +6,7 @@
 
 #define LOG
 #define DEBUG
-// #define TRACE
+#define TRACE
 
 // Log
 #ifdef LOG
--- src/inode_manager.c
+++ src/inode_manager.c
@@ -78,7 +78,9 @@ static void inode_recursive_print(inode_t *node, int depth) {
 }
 
 void inode_manager_init(inode_manager_t *inode_manager) {
+  TRACE_ENTRY;
   inode_manager->root = new_inode("/", INODE_DIR, DEFAULT_MODE);
+  TRACE_EXIT;
 }
 
 inode_t *inode_manager_lookup(inode_manager_t *inode_manager, const char *path, int flags) {
