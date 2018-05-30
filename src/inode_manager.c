763a41f02a2bb66d5a106cd9578b69fb574ef0ab
tracer <tracer@tracer.org>
2018-05-31 00:10:40 +0800

    > run ray Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux

--- src/inode_manager.c
+++ src/inode_manager.c
@@ -13,14 +13,12 @@ static inode_t *new_inode(const char *name, int type, int mode) {
 }
 
 static void delete_inode(inode_t *node) {
-  //inode_t *scan = node->child;
-  // while (scan != NULL) {
-  //   inode_t *save = scan->sibling;
-  //   delete_inode(scan);
-  //   scan = save;
-  // }
-  for (inode_t *scan = node->child; scan != NULL; scan = scan->sibling) 
+  inode_t *scan = node->child;
+  while (scan != NULL) {
+    inode_t *save = scan->sibling;
     delete_inode(scan);
+    scan = save;
+  }
   pmm->free(node);
   node->parent = node->child = node->sibling = NULL;
 }
