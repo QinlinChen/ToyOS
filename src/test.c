cfb2950707fbd125080c352e123256d7d77babbc
QinlinChen <chenqinlin98@gmail.com>
2018-06-03 16:40:38 +0800

    modify procfs_test

--- src/test.c
+++ src/test.c
@@ -506,7 +506,7 @@ int procfs_test() {
   strcat(path, name);
   strcat(path, "/hello");
 
-  Assert(vfs->open("/proc/cpuinfo", O_RDONLY) == -1);
+  Assert(vfs->open("/proc/vgainfo", O_RDONLY) == -1);
 
   int fd;
   size_t size;
